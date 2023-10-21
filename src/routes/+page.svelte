53f9d51945b4938a3d04222c51ddc13c7bec523b
Force Fidele KIEN <forcefidele@gmail.com>
2023-10-22 01:46:23 +0200

    fix: try to fix "Cannot access url.searchParams..." error
    Error: Cannot access url.searchParams on a page with prerendering enabled

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+    export const prerender = true;
+</script>
+
 <script lang="ts">
 	import SessionCard from '$lib/SessionCard.svelte';
 	import { onMount } from 'svelte';
