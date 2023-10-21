cd60bd44f96cbadb7fe2f3fa8d54c22755648576
Force Fidele KIEN <forcefidele@gmail.com>
2023-10-22 01:48:03 +0200

    fix: another try

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script context="module">
-    export const prerender = true;
-</script>
-
 <script lang="ts">
 	import SessionCard from '$lib/SessionCard.svelte';
 	import { onMount } from 'svelte';
