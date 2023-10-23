0bebe7c620ec89c3e61aeb4eedd7e37876f316e2
Force Fidele KIEN <forcefidele@gmail.com>
2023-10-23 13:10:14 +0200

    refactor: remove unused style

--- src/routes/session/+page.svelte
+++ src/routes/session/+page.svelte
@@ -204,16 +204,6 @@
 		box-sizing: border-box;
 	}
 
-	#loader {
-		width: 80px;
-		height: 40px;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		margin: -20px -40px;
-		z-index: 1000;
-	}
-
 	.one-liner {
 		display: flex;
 		align-items: center;
